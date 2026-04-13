54fad317c35b6fcc4ffd0df02944aabaec35173c
babycatoffical <arazzang880331@gmail.com>
2026-04-14 08:19:58 +0900

    feat. 일부 메서드에서 Player가 NonePlayer 조건을 만족할 경우, 해당 메서드를 탈출하도록 추가 clean. 코드 정리
    Signed-off-by: babycatoffical <arazzang880331@gmail.com>

--- RGM/RGM/EventArgs/PlayerEvents.cs
+++ RGM/RGM/EventArgs/PlayerEvents.cs
@@ -425,6 +425,8 @@ namespace RGM.EventArgs
 
         public static IEnumerator<float> OnLeft(LeftEventArgs ev)
         {
+            if (ev.Player.IsNonePlayer()) yield break;
+            
             if (TranslatorPlayers.ContainsKey(ev.Player))
                 TranslatorPlayers.Remove(ev.Player);
 
@@ -528,6 +530,8 @@ namespace RGM.EventArgs
 
         public static void OnSpawned(SpawnedEventArgs ev)
         {
+            if (ev.Player.IsNPC) return;
+            
             ev.Player.EnableEffect(EffectType.FogControl, 1);
 
             if (ev.Player.IsAlive)
@@ -668,6 +672,8 @@ namespace RGM.EventArgs
         
         public static void OnHurting(HurtingEventArgs ev)
         {
+            if (ev.Player.IsNonePlayer()) return;
+            
             if (ev.DamageHandler.Type == DamageType.Falldown && ev.Player.TryGetEffect(EffectType.Lightweight, out StatusEffectBase lightweight) && lightweight.IsEnabled)
             {
                 if (HolidayUtils.IsHolidayActive(HolidayType.Halloween) && ev.Player.TryGetEffect(EffectType.Metal, out StatusEffectBase metal) && metal.IsEnabled)
@@ -700,7 +706,8 @@ namespace RGM.EventArgs
 
         public static void OnDying(DyingEventArgs ev)
         {
-            if (Round.IsEnded)
+            if (Round.IsEnded ||
+                ev.Player.IsNonePlayer())
                 return;
 
             if (Round.IsLobby)
@@ -722,23 +729,25 @@ namespace RGM.EventArgs
                 }
                 else
                 {
-                    if (ev.DamageHandler.Type == DamageType.PocketDimension)
-                    {
-                        var attacker = Player.Get(RoleTypeId.Scp106).GetRandomValue();
+                    if (ev.DamageHandler.Type != DamageType.PocketDimension) return;
+                    var attacker = Player.Get(RoleTypeId.Scp106).GetRandomValue();
 
-                        if (attacker == null) return;
+                    if (attacker == null) return;
 
-                        ev.IsAllowed = false;
+                    ev.IsAllowed = false;
 
-                        ev.Player.Kill(new PlayerStatsSystem.ScpDamageHandler(attacker.ReferenceHub, DeathTranslations.PocketDecay));
-                    }
+                    ev.Player.Kill(new ScpDamageHandler(attacker.ReferenceHub, DeathTranslations.PocketDecay));
                 }
             }
         }
 
         public static void OnDied(DiedEventArgs ev)
         {
-            if (ev.Attacker == null || ev.Attacker.IsNonePlayer() || ev.Player.IsNonePlayer() || Round.IsEnded)
+            if (ev.Attacker == null ||
+                ev.Player == null ||
+                ev.Attacker.IsNonePlayer() ||
+                ev.Player.IsNonePlayer() ||
+                Round.IsEnded)
                 return;
 
             // 저장된 효과 삭제
@@ -759,8 +768,7 @@ namespace RGM.EventArgs
                     if (ev.Attacker == null)
                         return $"{(PlayersInfo.ContainsKey(ev.Player.UserId) && ev.DamageHandler.Type == DamageType.Unknown ? "⏳ <color=#FF0000><b>SCP 탈주</b></color>(3분 내로 재접속 가능)" : "💀 <color=#A4A4A4>자살</color>")}ㅣ{Tools.BadgeFormat(ev.Player)}<color=#F2F5A9>{ev.Player.DisplayNickname}</color>(<color={ev.TargetOldRole.GetColor().ToHex()}>{Trans.Role[ev.TargetOldRole]}</color>) - {ev.DamageHandler.Type}";
 
-                    else
-                        return $"💔 <color=#FAAC58>{(ev.Player.IsCuffed ? "<b>체포킬</b>(신고 가능 여부는 규칙 확인)" : "사살")}</color>ㅣ{Tools.BadgeFormat(ev.Attacker)}<color=#F2F5A9><i>{ev.Attacker.DisplayNickname}</i></color>(<color={ev.Attacker.Role.Color.ToHex()}>{Trans.Role[ev.Attacker.Role.Type]}</color>) -> {Tools.BadgeFormat(ev.Player)}<color=#F2F5A9>{ev.Player.DisplayNickname}</color>(<color={ev.TargetOldRole.GetColor().ToHex()}>{Trans.Role[ev.TargetOldRole]}</color>) - {ev.DamageHandler.Type}";
+                    return $"💔 <color=#FAAC58>{(ev.Player.IsCuffed ? "<b>체포킬</b>(신고 가능 여부는 규칙 확인)" : "사살")}</color>ㅣ{Tools.BadgeFormat(ev.Attacker)}<color=#F2F5A9><i>{ev.Attacker.DisplayNickname}</i></color>(<color={ev.Attacker.Role.Color.ToHex()}>{Trans.Role[ev.Attacker.Role.Type]}</color>) -> {Tools.BadgeFormat(ev.Player)}<color=#F2F5A9>{ev.Player.DisplayNickname}</color>(<color={ev.TargetOldRole.GetColor().ToHex()}>{Trans.Role[ev.TargetOldRole]}</color>) - {ev.DamageHandler.Type}";
                 }
 
                 foreach (var player in PlayerManager.List.Where(x => x.IsDead || x == ev.Attacker))
