e3dda6a8e4312194c4940333f35828e15f4e7027
babycatoffical <arazzang880331@gmail.com>
2026-04-14 01:09:13 +0900

    feat. ABattle 모드가 언로드될 경우, 중력 감소 10 효과를 없에는 기능 추가
    Signed-off-by: babycatoffical <arazzang880331@gmail.com>

--- RGM/RGM/Modes/Plus/ABattle/Abilities/Normal/Rabbit.cs
+++ RGM/RGM/Modes/Plus/ABattle/Abilities/Normal/Rabbit.cs
@@ -6,12 +6,15 @@ namespace RGM.Modes.Abilities.Normal;
 [Ability("토끼뜀", "점프력이 10% 증가합니다.", AbilityCategory.Common, AbilityType.NORMAL_RABBIT)]
 public class Rabbit : Ability
 {
+    private byte _count;
     public override void OnEnabled()
     {
         Owner.AddEffect(EffectType.Lightweight, 10);
+        _count += 1;
     }
 
     public override void OnDisabled()
     {
+        Owner.RemoveEffect(EffectType.Lightweight, 10 * _count);  
     }
 }
